9ca91434a8cd18a2b255331dcf3fa359237cd144
alex75ita <alessandro.piccione.75@gmail.com>
2015-09-29 02:31:52 +0200

    Corrected "save" test for Employees.

--- facades/userFacade.py
+++ facades/userFacade.py
@@ -6,4 +6,9 @@ class UserFacade:
         self.connection = None
 
     def save(self, employee):
+        #employee = Employee("AAA", "BBB")
+        #client = MongoClient(self.dbServer, self.dbPort)
+        #db = client[self.dbName]
+        #document = employee.toJson()
+        #id = db.employees.insert_one(document).inserted_id
         pass
--- tests/integrationTests/userFacade_test.py
+++ tests/integrationTests/userFacade_test.py
@@ -8,23 +8,35 @@ from pymongo.mongo_client import MongoClient
 class userFacadeTests(unittest.TestCase):
 
     def setUp(self):
+        print("setUp")
         self.userFacade = UserFacade()
         self.dbServer = "mongodb://213.136.84.88"
         self.dbPort = 27017
         self.dbName = "timesheets_test"
+        #self.employeesCollectionName = "employees"
+
+        client = MongoClient(self.dbServer, self.dbPort)
+        db = client[self.dbName]
+        db.employees.drop()
 
     def tearDown(self):
+        print("tearDown")
         # delete database
         pass
 
-    def test_save_should_works(self):
+    def test_save_should_create_record_in_database(self):
         employee = Employee("AAA", "CCC")
         self.userFacade.save(employee)
         #id = employee.Id
         #self.assertIsNotNone(id)
         #print(id)
 
-        pass
+        # Assert
+        client = MongoClient(self.dbServer, self.dbPort)
+        db = client[self.dbName]
+        loaded_employee = db.employees.find_one({"firstName":"AAA"})
+        self.assertIsNotNone(loaded_employee)
+
 
     def test_save_create_a_record_in_database(self):
         employee = Employee("AAA", "BBB")
