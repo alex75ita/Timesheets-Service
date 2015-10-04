807106b7c1fb5fffa61662f199a20f2dd63680ce
alex75ita <alessandro.piccione.75@gmail.com>
2015-10-04 16:16:42 +0200

    Added test class for ConsumerBase

--- tests/integrationTests/consumers/consumer_test.py
+++ tests/integrationTests/consumers/consumer_test.py
@@ -15,19 +15,22 @@ class Consumer_test(unittest.TestCase):
             print(body)
 
         def connectionTimeoutCallback():
-            print("connecionClosedCallback")
+            print("connectionTimeoutCallback")
 
         def _closeChannel(channel_):
             print("_closeChannel")
             time.sleep(1)
             print("close")
-            channel_.stop_consuming()
-            #channel_.close()
+            if channel_.is_open and not channel.is_closing:
+                channel_.stop_consuming()
+                print("stop_consuming")
+            else:
+                print("channel is closed")
 
         params = pika.URLParameters(_url)
         params.socket_timeout = 5
         connection = pika.BlockingConnection(params)
-        connection.add_timeout(2, connectionTimeoutCallback)
+        connection.add_timeout(10, connectionTimeoutCallback)  # force to close
         channel = connection.channel()
         channel.basic_consume(callback,
                               queue='test',
@@ -37,9 +40,6 @@ class Consumer_test(unittest.TestCase):
         t.start()
 
         print("start_consuming")
-        channel.start_consuming()  # start consuming (blocks)
+        channel.start_consuming()  # start consuming (loop)
         connection.close()
         print("end")
-
-#class ChannelCloseThread(threading.Thread):
-
