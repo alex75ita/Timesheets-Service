2aeb77dd967f5b6b9a1114cae46e86ce11d34b60
Alessandro Piccione <alex.75@tin.it>
2015-10-20 19:17:54 +0200

    Correct Configuration __init__

--- entities/configuration.py
+++ entities/configuration.py
@@ -3,10 +3,15 @@ class Configuration:
 
 
     def __init__(self, rabbitServer, rabbitHost, rabbitUser, rabbitPassword):
+
         assert rabbitServer is not None
         assert rabbitHost is not None
         assert rabbitUser is not None
         assert rabbitPassword is not None
+
         self.rabbitServer = rabbitServer
+        self.rabbitHost = rabbitHost
+        self.rabbitUser = rabbitUser
+        self.rabbitPassword = rabbitPassword
 
 
